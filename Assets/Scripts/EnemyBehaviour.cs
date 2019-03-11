90dabc2b29d2d6bd9a32ea8bbf80e8c9bf283055
Daniel Ariz <daniel.ariz.s@gmail.com>
2019-03-11 00:39:12 -0300

    Enemy Behaviours for different kinds of enemies

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public float speed;
+    public float offSet;
+
+    public float jumpForce;
+    public float jumpRate;
+    public float nextJump;
+
+    public Transform origin;
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        this.rb2d = this.GetComponent<Rigidbody2D>();
+    }
+
+    float IsMovingRight()
+    {
+        float rightLimit = this.origin.position.x + this.offSet;
+        float leftLimit = this.origin.position.x - this.offSet;
+        if (this.transform.position.x > rightLimit)
+        {
+            this.speed *= -1;
+        }
+        else if (this.transform.position.x < leftLimit)
+        {
+            this.speed *= -1;
+        }
+        return this.speed;
+    }
+
+    public void WalkPatrol() 
+    {
+        this.rb2d.velocity = this.transform.right * this.IsMovingRight();
+        this.ResetPosition();
+    }
+
+    public void JumpPatrol()
+    {
+        this.Jump();
+    }
+
+    void Jump()
+    {
+        if(Time.time > this.nextJump)
+        {
+            this.nextJump = Time.time + this.jumpRate;
+            this.rb2d.AddForce(new Vector2(this.IsMovingRight(), this.jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    void ResetPosition()
+    {
+        float rightLimit = this.origin.position.x + this.offSet;
+        float leftLimit = this.origin.position.x - this.offSet;
+        if(this.transform.position.x > rightLimit || this.transform.position.x < leftLimit)
+        {
+            Vector3.MoveTowards(this.transform.position, this.origin.position, 1f);
+        }
+    }
+}
