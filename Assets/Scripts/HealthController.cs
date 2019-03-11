a1213d5f71d607bd4cae8f4e8f7457c85a6bfbc5
Daniel Ariz <daniel.ariz.s@gmail.com>
2019-03-11 00:38:19 -0300

    Health Controller apart to reuse

---
+++ Assets/Scripts/HealthController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour
+{
+    public float maxHealth;
+    public float currentHealth;
+    void Start()
+    {
+        this.currentHealth = this.maxHealth;
+    }
+    public void TakeDamage(int damage)
+    {
+        this.currentHealth -= damage;
+
+        if(this.currentHealth <= 0)
+        {
+            this.currentHealth = 0;
+            this.Die();
+        }
+    }
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
