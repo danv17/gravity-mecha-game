64c9b09505e9c6d752126a5b6fffc4a241675d57
Daniel Ariz <daniel.ariz.s@gmail.com>
2019-03-20 13:41:36 -0300

    Utility scripts add to folder

---
+++ Assets/Scripts/Utilities/Mover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [Range(1, 10)]
+    public float speed;
+    void Start()
+    {
+        this.rb = this.GetComponent<Rigidbody2D>();
+        this.rb.velocity = this.transform.up * this.speed;
+    }
+}
---
+++ Assets/Scripts/Utilities/Shot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour
+{
+    public int damage;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+            return;
+
+        if (collision.CompareTag("Enemy"))
+        {
+            collision.GetComponent<EnemyController>().healthController.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
+}
